8c72737cda36cc7943a47172f61e0a5c99c6a4e4
Namita Roy <Namita.Roy@merkle.com>
2025-09-11 20:05:28 +0530

    add sitecore config

--- examples/kit-nextjs-skate-park/sitecore.config.ts
+++ examples/kit-nextjs-skate-park/sitecore.config.ts
@@ -4,4 +4,10 @@ import { defineConfig } from '@sitecore-content-sdk/nextjs/config';
  * See the documentation for `defineConfig`:
  * https://doc.sitecore.com/xmc/en/developers/content-sdk/the-sitecore-configuration-file.html
  */
-export default defineConfig({});
+export default defineConfig({
+    personalize: {
+    scope: process.env.NEXT_PUBLIC_PERSONALIZE_SCOPE,
+    edgeTimeout: parseInt(process.env.PERSONALIZE_MIDDLEWARE_EDGE_TIMEOUT!, 10),
+    cdpTimeout: parseInt(process.env.PERSONALIZE_MIDDLEWARE_EDGE_TIMEOUT!, 10),
+  },
+});
